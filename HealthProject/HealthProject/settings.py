c6c6ef4ad4d91b3059b857a270aa998a91ac73c2
sayanth123 <59867917+sayanth123@users.noreply.github.com>
2021-07-19 13:20:12 +0530

    Update settings.py

--- HealthProject/HealthProject/settings.py
+++ HealthProject/HealthProject/settings.py
@@ -65,6 +65,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'mainapp.context_processors.menu_links',
+                'mainapp.context_processors.doc_links',
+
             ],
         },
     },
