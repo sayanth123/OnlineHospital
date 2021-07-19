d3ade23227fdc9e701eb243852b3e0c95c7034c7
sayanth123 <59867917+sayanth123@users.noreply.github.com>
2021-07-19 13:07:21 +0530

    Update forms.py

--- HealthProject/mainapp/forms.py
+++ HealthProject/mainapp/forms.py
@@ -1,36 +1,47 @@
 
-from .models import Patient
-from django import forms
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.db import models
 from django.contrib.auth.models import User
+# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images', blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    department=models.ForeignKey(Department,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images', blank=True)
+    available = models.CharField(max_length=250, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 
-
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    password1 = forms.CharField(label='Enter password',
-                                widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm password',
-                                widget=forms.PasswordInput)
-
-    class Meta:
-	    model = User
-	    fields = ("username", "email", "password1", "password2")
-
-    help_text = {
-        "username": None,
-    }
-
-class PatientForm(forms.ModelForm):
-    class Meta:
-        model = Patient
-        fields = ("name","date","time","department","doctor","phone","message")
-        widgets = {
-            'date': forms.SelectDateWidget(),
-            'message': forms.Textarea(attrs={'style': 'height: 150px;width:400px'})
+TIME_CHOICES = (
+   ('10 AM - 1 PM', 'morning'),
+   ('2 PM - 5 PM', 'afternoon')
+)
 
 
+class Patient(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=250, unique=True)
+    date = models.DateField()
+    time = models.CharField(choices=TIME_CHOICES,max_length=250)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    phone=models.CharField(max_length=50)
+    message = models.CharField(max_length=300)
 
-        }
+
+    def __str__(self):
+        return  self.name
+
 
